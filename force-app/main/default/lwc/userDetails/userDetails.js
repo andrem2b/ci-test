b0ce22f53224f20a44082ad9efdfc9882c7f88a2
andrem2b <andrem2b@gmail.com>
2019-11-03 20:41:31 -0300

    updated lwc

--- force-app/main/default/lwc/userDetails/userDetails.js
+++ force-app/main/default/lwc/userDetails/userDetails.js
@@ -1,3 +1,35 @@
-import { LightningElement } from 'lwc';
+import {
+    LightningElement,
+    wire,
+    track
+} from 'lwc';
+import {
+    getRecord
+} from 'lightning/uiRecordApi';
 
-export default class userDetails extends LightningElement {}
+import USER_ID from '@salesforce/user/Id';
+
+import NAME_FIELD from '@salesforce/schema/User.Name';
+import EMAIL_FIELD from '@salesforce/schema/User.Email';
+
+
+export default class Userinfoexample extends LightningElement {
+    @track error;
+    @track email;
+    @track name;
+    @wire(getRecord, {
+        recordId: USER_ID,
+        fields: [NAME_FIELD, EMAIL_FIELD]
+    }) wireuser({
+        error,
+        data
+    }) {
+        if (error) {
+            this.error = error;
+        } else if (data) {
+            this.email = data.fields.Email.value;
+            this.name = data.fields.Name.value;
+        }
+    }
+
+}
